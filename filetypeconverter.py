7ba48ff9c40a97d165af6369eb64f35f393bf592
Josiah Schmidt <37901964+jnschmidt18@users.noreply.github.com>
2019-05-14 22:30:53 -0400

    Update filetypeconverter.py

--- filetypeconverter.py
+++ filetypeconverter.py
@@ -5,7 +5,7 @@ infile = open(infilename, 'r') #opens read only
 text = infile.read() #dumps contents into string
 infile.close() #close that file
 print(text) #show text is read
-outfilename=str(input("give a file name to output to, must be .html: ")) #gets name of file to write to
+outfilename=str(input("give a file name to output to, should be .html: ")) #gets name of file to write to
 outfile=open(outfilename, 'a+') #create needed html file, read and write access
 outfile.write(text) #write text to file
 outfile.close() #close file
